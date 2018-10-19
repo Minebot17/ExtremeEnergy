74fdb95202276ecbd876c981d723377e5dca257a
Eugene <serpinin@mail.ru>
2018-10-19 18:14:54 +0300

    Забыл чирик!

--- src/main/java/ru/minebot/extreme_energy/Reference.java
+++ src/main/java/ru/minebot/extreme_energy/Reference.java
@@ -3,7 +3,7 @@ package ru.minebot.extreme_energy;
 public class Reference {
     public static final String MOD_ID = "meem";
     public static final String NAME = "Extreme Energy";
-    public static final String VERSION = "1.1.7";
+    public static final String VERSION = "1.1.8";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 
     public static final String CLIENT_PROXY_CLASS = "ru.minebot.extreme_energy.proxy.ClientProxy";
